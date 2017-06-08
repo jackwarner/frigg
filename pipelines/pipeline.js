2eae8ff464c2219009f2a475f7beca40d7a49696
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-06-08 17:23:21 -0400

    update child pipeline naming convention

--- pipelines/pipeline.js
+++ pipelines/pipeline.js
@@ -5,9 +5,9 @@ const Bash = require('../lib/bash');
 class Pipeline {
 
   constructor(repo) {
+    this.repo = repo;
     this.templateDirectory = `pipelines/templates/${repo.pipeline.name}/v${repo.pipeline.version}`;
-    this.tempDirectory = `/tmp/pipeline`
-    this.branch = repo.branch;
+    this.tempDirectory = `/tmp/pipeline`;
     this.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
     this.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
     this.bash = new Bash();
@@ -16,7 +16,7 @@ class Pipeline {
   deploy() {
     log.trace('Deploying pipeline');
     let command = `cp ${this.templateDirectory} -R ${this.tempDirectory}/ && chmod -R 777 ${this.tempDirectory}`;
-    command += ` && cd ${this.tempDirectory} && export AWS_ACCESS_KEY_ID=${this.AWS_ACCESS_KEY_ID} && export AWS_SECRET_ACCESS_KEY=${this.AWS_SECRET_ACCESS_KEY} && export HOME=${this.tempDirectory} && export STAGE=${this.branch}`;
+    command += ` && cd ${this.tempDirectory} && export AWS_ACCESS_KEY_ID=${this.AWS_ACCESS_KEY_ID} && export AWS_SECRET_ACCESS_KEY=${this.AWS_SECRET_ACCESS_KEY} && export HOME=${this.tempDirectory} && export STAGE=${this.repo.branch} && export REPO=${this.repo.name}`;
     command += ` && npm i && npm run deploy`;
     return this.bash.execute(command);
   }
