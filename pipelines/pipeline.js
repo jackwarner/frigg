b8ad46464a45bd4efb5644b37aaf345338c20b67
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-06-08 17:42:18 -0400

    add pipeline name as parameter

--- pipelines/pipeline.js
+++ pipelines/pipeline.js
@@ -6,6 +6,7 @@ class Pipeline {
 
   constructor(repo) {
     this.repo = repo;
+    this.pipelineServiceName = `${repo.name}-pipeline-${repo.branch}`;
     this.templateDirectory = `pipelines/templates/${repo.pipeline.name}/v${repo.pipeline.version}`;
     this.tempDirectory = `/tmp/pipeline`;
     this.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
@@ -16,7 +17,7 @@ class Pipeline {
   deploy() {
     log.trace('Deploying pipeline');
     let command = `cp ${this.templateDirectory} -R ${this.tempDirectory}/ && chmod -R 777 ${this.tempDirectory}`;
-    command += ` && cd ${this.tempDirectory} && export AWS_ACCESS_KEY_ID=${this.AWS_ACCESS_KEY_ID} && export AWS_SECRET_ACCESS_KEY=${this.AWS_SECRET_ACCESS_KEY} && export HOME=${this.tempDirectory} && export STAGE=${this.repo.branch} && export REPO=${this.repo.name}`;
+    command += ` && cd ${this.tempDirectory} && export AWS_ACCESS_KEY_ID=${this.AWS_ACCESS_KEY_ID} && export AWS_SECRET_ACCESS_KEY=${this.AWS_SECRET_ACCESS_KEY} && export HOME=${this.tempDirectory} && export STAGE=${this.repo.branch} && export PIPELINE_SERVICE_NAME=${this.pipelineServiceName}`;
     command += ` && npm i && npm run deploy`;
     return this.bash.execute(command);
   }
