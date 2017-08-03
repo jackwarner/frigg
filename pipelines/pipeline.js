acf5955857efc0292246c27c09b8a1d91b76e178
Phillip Manwaring <phillip.manwaring@gmail.com>
2017-08-03 11:47:24 -0400

    add in pipeline creation flow

--- pipelines/pipeline.js
+++ pipelines/pipeline.js
@@ -5,7 +5,6 @@ const sns = new AWS.SNS({ apiVersion: '2010-03-31' });
 const cf = new AWS.CloudFormation({ apiVersion: '2010-05-15' });
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 const codebuild = new AWS.CodeBuild({ apiVersion: '2016-10-06' });
-const Bash = require('../lib/bash');
 const Zip = require('../lib/zip');
 const log = require('../lib/log');
 
@@ -15,34 +14,17 @@ class Pipeline {
     log.info('Creating pipeline from config', config);
     this.config = config;
     this.templateDirectory = `pipelines/templates/${config.pipeline.name}/v${config.pipeline.version}`;
-    this.tempDirectory = `/tmp/pipeline`;
-    this.codeBuildArtifact = `${this.tempDirectory}/zip/build.zip`;
-    this.AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-    this.AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-    this.bash = new Bash();
   }
 
   deploy() {
+    log.info('Deploying pipeline');
     return createBuildArtifact()
       .then(uploadBuildArtifact)
-      .then(triggerBuild)
-    // TODO create zip with pipeline and params
-    // upload zip to bucket with versioning
-    // trigger codebuild with version
-    
-
-    
-    log.info('Deploying pipeline');
-    const repository = this.config.repository, pipeline = this.config.pipeline;
-    // TODO clean this up and mask access key / tokens when bash command is logged
-    let command = `cp ${this.templateDirectory} -R ${this.tempDirectory}/ && chmod -R 777 ${this.tempDirectory}`;
-    command += ` && cd ${this.tempDirectory} && export AWS_ACCESS_KEY_ID=${this.AWS_ACCESS_KEY_ID} && export AWS_SECRET_ACCESS_KEY=${this.AWS_SECRET_ACCESS_KEY}`;
-    command += ` && export HOME=${this.tempDirectory} && export STAGE=${pipeline.stage} && export PIPELINE_SERVICE_NAME=${pipeline.serviceName} && export REPO_NAME=${repository.fullyQualifiedName} && export OWNER=${repository.owner} && export REPO=${repository.name} && export BRANCH=${repository.branch}`;
-    command += ` && npm i && npm run deploy`;
-    return this.bash.execute(command).then(res => this.emitPipelineAdded());
+      .then(triggerBuild);
   }
 
   createBuildArtifact() {
+    log.info('Creating build artifact');
     let zip = new Zip(this.templateDirectory);
     return Promise.resolve(zip);
   }
@@ -50,17 +32,19 @@ class Pipeline {
   uploadBuildArtifact(zip) {
     const params = {
       Body: zip.toBuffer(),
-      Bucket: '',
-      Key: ''
+      Bucket: process.env.BUILD_ARTIFACT_BUCKET,
+      Key: 'build.zip'
     };
+    log.info('Uploading build artifact with params', params);
     s3.putObject(params).promise();
   }
 
   triggerBuild(artifactData) {
     const params = {
-      projectName: '',
+      projectName: process.env.BUILD_PROJECT_NAME,
       sourceVersion: artifactData.VersionId
     };
+    log.info('Triggering build with params', params);
     codebuild.startBuild(params).promise();
   }
 
